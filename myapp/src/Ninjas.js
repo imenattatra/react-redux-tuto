a818cbc3902e17b5097483a0f4d39b83b6d053aa
zolicoeur <ci_attatra@esi.dz>
2020-05-21 14:55:17 +0200

    add ternary condition

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -3,13 +3,14 @@ import React from 'react';
 const Ninjas =({ninjas})=>{
 
     const ninjaList=ninjas.map(ninja=>{
-        return(
+        return ninja.age>13 ? (
             <div className="ninja" key={ninja.id}>
                 <div>Name : {ninja.name}</div>
                 <div>Age : {ninja.age}</div>
                 <div>Belt : {ninja.belt}</div>
             </div>
-        );
+        ):null;
+
     })
     return(
         <div className="ninjas-list">
