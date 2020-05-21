8878129d4aecd10e52095a3d56a2930a4a18bd46
zolicoeur <ci_attatra@esi.dz>
2020-05-21 14:10:37 +0200

    destructuring props and reuse component

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 
 class Ninjas extends Component{
     render(){
+        
+        const {name,age,belt}=this.props;//destructuring
         return(
             <div className="ninja">
-                <div>Name : {this.props.name}</div>
-                <div>Age : {this.props.age}</div>
-                <div>Belt : {this.props.belt}</div>
+                <div>Name : {name}</div>
+                <div>Age : {age}</div>
+                <div>Belt : {belt}</div>
             </div>
         );
     };
