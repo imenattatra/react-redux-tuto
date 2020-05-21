9036de8a7ab1cec58732aa0f7b1d9a0370c51a90
zolicoeur <ci_attatra@esi.dz>
2020-05-21 14:41:55 +0200

    use stateless component/function component with direct destructing of props

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -1,25 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Ninjas extends Component{
-    render(){
-        
-        const {ninjas}=this.props;//destructuring
-        const ninjaList=ninjas.map(ninja=>{
-            return(
-                <div className="ninja" key={ninja.id}>
-                    <div>Name : {ninja.name}</div>
-                    <div>Age : {ninja.age}</div>
-                    <div>Belt : {ninja.belt}</div>
-                </div>
-            );
-        })
+const Ninjas =({ninjas})=>{
+
+    const ninjaList=ninjas.map(ninja=>{
         return(
-            <div className="ninjas-list">
-                {ninjaList}
+            <div className="ninja" key={ninja.id}>
+                <div>Name : {ninja.name}</div>
+                <div>Age : {ninja.age}</div>
+                <div>Belt : {ninja.belt}</div>
             </div>
-        )
+        );
+    })
+    return(
+        <div className="ninjas-list">
+            {ninjaList}
+        </div>
+    )
 
-    };
 }
 
 export default Ninjas;
