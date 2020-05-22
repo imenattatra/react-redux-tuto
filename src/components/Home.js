ba694f35303c8bb29980b85c7395f122e660296c
zolicoeur <ci_attatra@esi.dz>
2020-05-22 18:01:27 +0200

    adding components

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home=()=> {
+    return (
+        
+        <div className="container">
+            <h4 className="center">Home</h4>
+            <p>Loren Ipsum</p>
+        </div>
+
+    );
+  }
+  
+  export default Home;
