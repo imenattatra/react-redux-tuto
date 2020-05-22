a1027464154468300f6de24ffea48fc4059c3c42
zolicoeur <ci_attatra@esi.dz>
2020-05-22 18:49:00 +0200

    set up router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
-import NavBar from './components/NavBar'
+import NavBar from './components/NavBar';
+import { BrowserRouter , Route} from 'react-router-dom'
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <NavBar />
+        <Route exact path='/' component={Home} />
+        <Route path='/about' component={About} />
+        <Route path='/contact' component={Contact} />
+      </div>
+    </BrowserRouter>
+
   );
 }
 
